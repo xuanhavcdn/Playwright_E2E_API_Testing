ce7ec252cfb237d56d480e650072116488e9d7ec
xuanhavcdn <xuanhavcdn@gmail.com>
2023-08-12 15:41:12 +0700

    Revert "Develop"

--- src/test/features/apitesting.feature
+++
@@ -1,5 +0,0 @@
-Feature: API testing
-  Scenario: Test menu list
-    Given The api is up and running
-    When I get list bank
-    Then Verify list bank api repsonse correctly with sucess status
--- src/test/pages/apiTesting.ts
+++
@@ -1,8 +0,0 @@
-const apiTesting =  {
-    base_url: "https://web.v2-stg.thuocsi.vn",
-    api_signin: "/backend/marketplace/customer/v1/authentication",
-    get_list_seller: "/api/seller",
-    get_listbank: "/backend/marketplace/customer/v1/me/bank/list"
-}
-
-export default apiTesting
