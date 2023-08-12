0848fa20317e9521f232d741f27e3fa45631553a
Ha Cao <xuanhavcdn@gmail.com>
2023-08-12 12:37:17 +0700

    api testing

--- src/test/steps/apiTesing.ts
+++ src/test/steps/apiTesing.ts
@@ -2,30 +2,39 @@ import {Given, When, Then} from "@cucumber/cucumber"
 import apiTesting from "../pages/apiTesting"
 import {chromium, Page, Browser, expect, request} from "@playwright/test"
 const axios = require('axios');
-
+ 
 const apidata = require("../fixture/apiTesting")
 let browser: Browser
 let pages: Page
 
 Given('The api is up and running',  async function () {
-  this.response = await axios.post({
-    url: apiTesting.api_token,
-    data: {
-        "UserAccount": apidata.Username,
-        "Password": apidata.Password
+  //route api token
+  this.routeApiResponse = await axios.post(
+    apiTesting.base_url+ apiTesting.api_signin,
+    {
+      username: apidata.username,
+      password: apidata.password,
+      type: apidata.type
     }
-})
-  expect(this.response.status()).toBe(200)
-})
-
-When('I get menu list', async function() {
-  // Write code here that turns the phrase above into concrete actions
+    )
+  expect(this.routeApiResponse.status).toBe(200)
+  // console.log(this.routeApiResponse.data.data[0].bearerToken)
+  this.token = this.routeApiResponse.data.data[0].bearerToken
 })
 
-Then('The menu list should be response successfully with correct schema', async function() {
-  // Write code here that turns the phrase above into concrete actions
+When('I get list bank', async function() {
+  // console.log(`Bearer ${token}`)
+  this.accessTokenResponse = await axios.get(
+    apiTesting.base_url + apiTesting.get_listbank,
+    {
+      headers: {
+        Authorization: `Bearer ${this.token}`
+      }
+    }
+  )
 })
 
-Then('The menu list should be response successfully with status code {int}', async function (int: number) {
-  // Write code here that turns the phrase above into concrete actions
+Then('Verify list bank api repsonse correctly with sucess status', async function() {
+  expect(this.accessTokenResponse.status).toBe(200)
+  // console.log(this.accessTokenResponse.data)
 })
